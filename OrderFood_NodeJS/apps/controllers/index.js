4210a9d091a54f1ff7d85cfc9b8d938d1066d95f
Dazk <phungxuandoan2001@gmail.com>
2023-03-05 14:09:55 +0700

    Tiêu đề: Khởi động dự án Nội dung: sửa index

--- OrderFood_NodeJS/apps/controllers/index.js
+++ OrderFood_NodeJS/apps/controllers/index.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var router = express.Router();
 router.get("/", function(req,res){
-    res.json({"message": "this is index page"});
+    res.json({"message": "đây là home pages"});
 });
 module.exports = router;
