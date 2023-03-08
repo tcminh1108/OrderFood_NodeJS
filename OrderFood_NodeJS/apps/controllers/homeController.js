0c66ccf9b7ac340049559e069c2c625fb3ef764d
TCMINH <tcminh1108@gmail.com>
2023-03-08 10:53:11 +0700

    Tiêu đề: edit HomeController /  Nội dụng: call API lấy trang login và register

--- OrderFood_NodeJS/apps/controllers/homeController.js
+++ OrderFood_NodeJS/apps/controllers/homeController.js
@@ -4,4 +4,14 @@ var router = express.Router();
 router.get("/", function(req,res){
     res.render("homeView/home");
 });
+
+//Hiển thị trang login
+router.get("/", function(req,res){
+    res.render("homeView/Login");
+});
+
+//Hiển thị trang register
+router.get("/", function(req,res){
+    res.render("homeView/register");
+});
 module.exports = router;
