5acc16e0b2af93ee7f41da5c7cd9fba8cb429d2c
Dazk <phungxuandoan2001@gmail.com>
2023-03-15 13:51:50 +0700

    Tiêu đề: edit giao diện css

--- OrderFood_NodeJS/apps/controllers/index.js
+++ OrderFood_NodeJS/apps/controllers/index.js
@@ -5,6 +5,7 @@ router.get("/", function(req,res){
     res.json({"message": "đây là home pages"});
     res.json({"message": "xin chao"});
     res.json({"message": "xin chao ngay 2"});
+    res.json({"message": "xin chao ngay 3"});
 });
 module.exports = router;
 //
