a35d9db9463f74eb0c7293c625675a4f1acea985
Dazk <phungxuandoan2001@gmail.com>
2023-03-16 22:26:43 +0700

    tiêu đê update ngày 16.3

--- OrderFood_NodeJS/apps/controllers/index.js
+++ OrderFood_NodeJS/apps/controllers/index.js
@@ -3,9 +3,7 @@ var router = express.Router();
 router.get("/", function(req,res){
     res.json({"message": "đây là home pages"});
     res.json({"message": "đây là home pages"});
-    res.json({"message": "xin chao"});
-    res.json({"message": "xin chao ngay 2"});
-    res.json({"message": "xin chao ngay 3"});
+    //commetn o days
 });
 module.exports = router;
 //
