2422aab6f1bc0f4597312ceafef6c478ab6069f9
Dazk <phungxuandoan2001@gmail.com>
2023-03-19 13:03:36 +0700

    edit HTML

--- OrderFood_NodeJS/apps/controllers/index.js
+++ OrderFood_NodeJS/apps/controllers/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 router.get("/", function(req,res){
     res.json({"message": "đây là home pages"});
     res.json({"message": "đây là home pages"});
+    res.json({"xinchao"});
     //commetn o days
 });
 module.exports = router;
