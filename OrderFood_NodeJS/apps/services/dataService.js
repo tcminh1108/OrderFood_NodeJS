a8d85161ea9f568f93c2e03fa8510e3ac4f49b25
huynhhaidang8@gmail.com <90547689+dangharold@users.noreply.github.com>
2023-03-27 01:26:01 +0700

    Tiêu đề: Chỉnh sửa file Nội dung: thêm chức năng lấy thức ăn theo ID, lấy tất cả món ăn

--- OrderFood_NodeJS/apps/services/dataService.js
+++ OrderFood_NodeJS/apps/services/dataService.js
@@ -14,4 +14,22 @@ class DataService{
         this.categoriesCollection = this.database.collection("categories");
         this.foodsCollection = this.database.collection("foods");
     }
+     //lấy món ăn theo id
+     async getFoodById(food_id) {
+        return await this.foodsCollection.findOne({"_id": new ObjectId(food_id)},{});
+    }
+    //lấy danh sách loại món ăn
+    async getCategoryList() {
+        const cursor = await this.categoriesCollection.find({}, {}).skip(0).limit(100);
+        return await cursor.toArray();
+    }
+    //lấy loại món ăn theo id
+    async getCategoryById(category_id) {
+        return await this.categoriesCollection.findOne({"_id": new ObjectId(category_id)},{});
+    }
+    //lấy danh sách món ăn
+    async getFoodList() {
+        const cursor = await this.foodsCollection.find({}, {}).skip(0).limit(100);
+        return await cursor.toArray();
+    }
 }
